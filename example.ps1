c770aba7de6983acd27ad04a3e7d56276658a0e9
Archaedas <am@hades.so>
2023-05-19 17:00:41 -0400

    Regenerate Pages

--- example.ps1
+++ example.ps1
@@ -1,18 +1,11 @@
-Import-Module .\src\PosHTML.psm1
+import-module .\src\poshtml.psm1
 
-<#
-    Params:
-        In : string
-            Path to your markdown file
-        Out : string
-            Desired HTML file output path
-        Template : string | $null
-            Optional HTML templage file path - the first string formatting token - {0} - will be targeted for translated HTML
-#>
-# ConvertTo-PosHTML -In .\markdown\example.md -Out .\html\example.html
+$siteParams    = @{
+    docroot    = 'D:\Cloud\personal\Profile\Documents\Repositories\PosHTML\markdown'
+    outdir     = 'D:\Cloud\personal\Profile\Documents\Repositories\PosHTML\docs' }
 
 # generate the site
-new-poshtmlsite -docroot 'D:\Cloud\personal\Profile\Documents\Repositories\PosHTML\markdown' -outdir 'D:\Cloud\personal\Profile\Documents\Repositories\PosHTML\docs' -pathprefix '/PosHTML'
+new-poshtmlsite  @siteParams
 
 # copy the stylesheet
-copy-item '.\src\style.css' '.\docs\style.css'
+copy-item '.\src\style.css' '.\docs\assets\css\style.css'
